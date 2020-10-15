420938f3bb63873c4ee2d8ce0392b14c28a21ac1
Ahmed Lotfy <lotfyah@yahoo.com>
2020-10-15 06:50:42 -0500

    Add files via upload
    adding the image load to model.py file

--- model.py
+++ model.py
@@ -29,6 +29,30 @@ class PredictFlower:
 
         self.load("model_name.h5")
 
-    
+    def call_predict(self, images, folder):
+
+        predictions = []
+
+        for image_name in images:
+            image_path = folder + "/" + image_name
+            print(f"imagepath: {image_path}")
+            test_image = keras.preprocessing.image.load_img(
+                image_path, target_size=(180, 180), grayscale=False)
+            test_image = image.img_to_array(test_image)
+            test_image = np.expand_dims(test_image, axis=0)
+            test_image = preprocess_input(test_image)
+
+            predict = self.model_final.predict(test_image)
+
+            zip_pred = zip(predict[0], self.category)
+
+            match_found = False
+            for pred_value, pred in zip_pred:
+                if (pred_value > 0.8):
+                    match_found = True
+                    predictions.append((image_name, pred))
+
+            if(not(match_found)):
+                predictions.append((image_name, ""))
 
         return predictions
